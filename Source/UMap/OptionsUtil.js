55019d7d6fb562fc9a42ff4098d053a05c80b8b6
JoJoeGoing <324518031@qq.com>
2019-02-13 11:03:45 +0800

    新增绘制操作时共有属性

--- Source/UMap/OptionsUtil.js
+++ Source/UMap/OptionsUtil.js
@@ -1,51 +1,65 @@
 define(['../Core/Cartesian2',
-        '../Core/Cartesian3',
-        '../Scene/VerticalOrigin',
-        '../Scene/HorizontalOrigin',
-        '../Core/Ellipsoid',
-        '../Core/VertexFormat'
+    '../Core/Cartesian3',
+    '../Scene/VerticalOrigin',
+    '../Scene/HorizontalOrigin',
+    '../Core/Ellipsoid',
+    '../Core/VertexFormat',
+    '../Core/Color'
 
-], function(Cartesian2, Cartesian3, VerticalOrigin, HorizontalOrigin,Ellipsoid,VertexFormat) {
+], function (Cartesian2, Cartesian3, VerticalOrigin, HorizontalOrigin, Ellipsoid, VertexFormat,Color) {
     'use strict';
 
     var OptionsUtil = {};
 
     OptionsUtil.billboard = {
-        verticalOrigin : VerticalOrigin.BOTTOM,
-        horizontalOrigin : HorizontalOrigin.CENTER,
-        pixelOffset : Cartesian2.ZERO,
-        scale : 1.0,
-        width : undefined,
-        height : undefined
+        verticalOrigin: VerticalOrigin.BOTTOM,
+        horizontalOrigin: HorizontalOrigin.CENTER,
+        pixelOffset: Cartesian2.ZERO,
+        scale: 1.0,
+        width: undefined,
+        height: undefined
     };
 
     OptionsUtil.label = {
-        fillColor : undefined,
-        font : undefined,
-        scale : 1.0,
-        show : true,
-        verticalOrigin : VerticalOrigin.BOTTOM,
-        horizontalOrigin : HorizontalOrigin.CENTER,
-        showBackground : false,
-        backgroundColor : undefined,
-        backgroundPadding : undefined,
-        outlineColor : undefined,
-        outlineWidth : undefined,
-        pixelOffset : undefined,
-        style : undefined
+        fillColor: undefined,
+        font: undefined,
+        scale: 1.0,
+        show: true,
+        verticalOrigin: VerticalOrigin.BOTTOM,
+        horizontalOrigin: HorizontalOrigin.CENTER,
+        showBackground: false,
+        backgroundColor: undefined,
+        backgroundPadding: undefined,
+        outlineColor: undefined,
+        outlineWidth: undefined,
+        pixelOffset: undefined,
+        style: undefined
     };
 
-    OptionsUtil.circle ={
-        center : Cartesian3.ZERO,
-        radius : 0.1,
+    OptionsUtil.circle = {
+        center: Cartesian3.ZERO,
+        radius: 0.1,
+        ellipsoid: Ellipsoid.WGS84,
+        height: 0,
+        granularity: 0.02,
+        extrudedHeight: 0,
+        stRotation: 0,
+        vertexFormat: VertexFormat.DEFAULT
+
+    };
+
+    OptionsUtil.drawOptions = {
+        show: true,
+        debugShowBoundingVolume: false,
+        asynchronous : false,
         ellipsoid : Ellipsoid.WGS84,
-        height : 0,
-        granularity : 0.02,
-        extrudedHeight : 0,
-        stRotation : 0,
-        vertexFormat : VertexFormat.DEFAULT
+        editable : false,
+        width : 5,
+        queryPrimitive : false,
+        color : new Color(0, 1, 1, 0.5)
 
     };
+
 
     return OptionsUtil;
 });
